8b71b1c3af9d65b031c38d68da19a8e89fcfd746
gardner-bp <sam.gardner@bridgerphotonics.com>
2017-01-20 16:20:58 -0700

    Added cmake scripts for out of source Win32 and Win64 builds

---
+++ make32.bat
@@ -0,0 +1,4 @@
+mkdir build_win32
+cd build_win32
+cmake -G "Visual Studio 14 2015" -D ARCH=Win32 ../ 
+cd ..
---
+++ make64.bat
@@ -0,0 +1,4 @@
+mkdir build_win64
+cd build_win64
+cmake -G "Visual Studio 14 2015 Win64" -D ARCH=Win64 ../ 
+cd ..
